5c09dea97400c5ae7e0e7a3fa5889641ce710020
fpofinet <lionelkanon67@gmail.com>
2023-12-17 12:46:03 +0100

    modification affichage  resultat

--- src/Controller/ResultatController.php
+++ src/Controller/ResultatController.php
@@ -18,10 +18,14 @@ class ResultatController extends AbstractController
         $province=$manager->getRepository(Province::class)->findAll();
         $provinces=array();
         $national=array();
+        $totalBv=0;
+        $totalRes=0;
         foreach($province as $p){
-            $prov=array(); $votant=0;$sf=0; $vo=0; $vn=0; $sn=0;
+            $prov=array(); $votant=0;$sf=0; $vo=0; $vn=0; $sn=0;$tb=0;$tr=0;
             foreach($p->getDepartements() as $d){
                 foreach($d->getCommunes() as $c){
+                    $tb=$tb+$c->getNombreBureau();
+                    $tr=$tr+count($c->getResultats());
                     foreach ($c->getResultats() as $r) {
                         if ($r->getEtat() == 0) {
                             $votant = $votant + $r->getVotant();
@@ -33,7 +37,9 @@ class ResultatController extends AbstractController
                     }     
                 }
             }
-            $prov=["id"=>$p->getId(),"libelle"=>$p->getLibelle(),"votant"=>$votant,"suffrageExprime"=>$sf,"suffrageNul"=>$sn,"voteOui"=>$vo,"voteNon"=>$vn];
+            $totalBv=$totalBv+$tb;
+            $totalRes=$totalRes+$tr;
+            $prov=["id"=>$p->getId(),"libelle"=>$p->getLibelle(),"votant"=>$votant,"suffrageExprime"=>$sf,"suffrageNul"=>$sn,"voteOui"=>$vo,"voteNon"=>$vn,"depouillement"=>(($tr/$tb)*100)];
             $provinces[]=$prov; 
         }
         $v=0;$se=0;$bn=0;$o=0;$n=0;
@@ -45,7 +51,7 @@ class ResultatController extends AbstractController
             $n= $n + $p["voteNon"];
            
         }
-        $national=["votant"=>$v,"suffrageExprime"=>$se,"suffrageNul"=>$bn,"voteOui"=>$o,"voteNon"=>$n];
+        $national=["votant"=>$v,"suffrageExprime"=>$se,"suffrageNul"=>$bn,"voteOui"=>$o,"voteNon"=>$n,"depouillement"=>(($totalRes/$totalBv)*100)];
         return $this->render('resultat/index.html.twig', [
             'province' => $provinces,
             'national' =>$national
@@ -66,7 +72,10 @@ class ResultatController extends AbstractController
             $sn = 0;
             $bvd = 0;
             $bv = 0;
+            $tb=0;$tr=0;
             foreach ($p->getCommunes() as $c) {
+                $tb=$tb+$c->getNombreBureau();
+                $tr=$tr+count($c->getResultats());
                 foreach ($c->getResultats() as $r) {
                     if ($r->getEtat() == 0) {
                         $votant = $votant + $r->getVotant();
@@ -77,7 +86,7 @@ class ResultatController extends AbstractController
                     }
                 }   
             }
-            $dep = ["id" => $p->getId(), "libelle" => $p->getLibelle(), "votant" => $votant, "suffrageExprime" => $sf, "suffrageNul" => $sn, "voteOui" => $vo, "voteNon" => $vn, "bvd" => $bvd, "bv" => $bv];
+            $dep = ["id" => $p->getId(), "libelle" => $p->getLibelle(), "votant" => $votant, "suffrageExprime" => $sf, "suffrageNul" => $sn, "voteOui" => $vo, "voteNon" => $vn, "bvd" => $bvd, "bv" => $bv,"depouillement"=>(($tr/$tb)*100)];
             $depart[] = $dep;
         }
         return $this->render('resultat/detailsProvince.html.twig', [
@@ -98,6 +107,9 @@ class ResultatController extends AbstractController
             $vo = 0;
             $vn = 0;
             $sn = 0;
+            $tb=0;$tr=0;
+            $tb=$tb+$c->getNombreBureau();
+            $tr=$tr+count($c->getResultats());
             foreach ($c->getResultats() as $r) {
                 if ($r->getEtat() == 0) {
                     $votant = $votant + $r->getVotant();
@@ -107,7 +119,7 @@ class ResultatController extends AbstractController
                     $vn = $vn + $r->getVoteNon();
                 }
             }
-            $dep = ["id" => $c->getId(), "libelle" => $c->getLibelle(), "votant" => $votant, "suffrageExprime" => $sf, "suffrageNul" => $sn, "voteOui" => $vo, "voteNon" => $vn];
+            $dep = ["id" => $c->getId(), "libelle" => $c->getLibelle(), "votant" => $votant, "suffrageExprime" => $sf, "suffrageNul" => $sn, "voteOui" => $vo, "voteNon" => $vn,"depouillement"=>(($tr/$tb)*100)];
             $comm[] = $dep;
         }
         return $this->render('resultat/detailsDepartement.html.twig', [
